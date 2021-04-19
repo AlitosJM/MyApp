dbdb484920152cd5099a3eb9c87f60b5750253f0
AliTosJM <alitosjm@gmail.com>
2021-04-18 20:20:55 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import Intro from "./Intro";
+
+function App() {
+  return (
+    <div>
+
+      {/*<Header />*/}
+      {/*<Note />*/}
+      <Intro />
+      {/*<Footer />*/}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Intro.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Intro() {
+  return (
+    <div className="is-preload">
+
+      <div id="wrapper">
+        <section id="main">
+
+          <header>
+            <span className="avatar"><img src="Jmat2.jpg" alt="" /></span>
+            <h1><em>@liTos</em></h1>
+            <p>Ing. Electrónico</p>
+          </header>
+
+          <footer>
+            <ul className="icons">
+              <li><a href="" className="icon solid fa-robot">Robot</a></li>
+            </ul>
+          </footer>
+
+        </section>
+
+        <footer id="footer">
+          <ul className="copyright">
+            <li>&copy; JMAT </li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+          </ul>
+        </footer>
+      </div>
+
+    </div>
+  );
+}
+
+export default Intro;
