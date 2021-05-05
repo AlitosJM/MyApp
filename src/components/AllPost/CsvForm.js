59611fb275436a5d1e21e697ade0a42d6b998a18
AliTosJM <alitosjm@gmail.com>
2021-05-05 18:36:45 -0500

    remains to be done save the file

--- src/components/AllPost/CsvForm.js
+++ src/components/AllPost/CsvForm.js
@@ -4,10 +4,34 @@ import React, { Component } from 'react';
 class MyForm extends Component {
   constructor(props) {
     super(props);
-    this.state = { username: '' };
+
+    this.state = {file: null}
   }
-  myChangeHandler = (event) => {
-    this.setState({username: event.target.value});
+
+  handleFileChange = (e) => {
+    this.setState({[e.target.name]: e.target.files[0],})
+  }
+
+  handleSubmit = async e => {
+  e.preventDefault();
+
+  const formData = new FormData();
+  for (let name in this.state) {
+    console.log(name)
+    console.log(this.state[name])
+    formData.append(name, this.state[name]);
+  }
+
+  /*await fetch('/api/upload', {
+    method: 'POST',
+    body: formData,
+  });*/
+
+  alert('done');
+}
+
+  myChangeHandler = (e) => {
+    this.setState({name: e.target.value});
   }
 
 
@@ -19,16 +43,19 @@ class MyForm extends Component {
       textDecoration: "underline"
     };
 
+
     return (
       <div>
-        <h1>Hello {this.state.username}</h1>
-        <form>          
+
+        {/*<h1>Hello {this.state.name}</h1>*/}
+        <form onSubmit={this.handleSubmit}>
             <p><span style={mystyle}>CSV</span>
-            <input type="file" name="file" required="required"></input>
-            <input type="submit" value="Guardar" class="btn btn-primary btn-block btn-large"></input>
-            </p>          
-          
-        </form> 
+            {/*<input type='text' />*/}
+            <input type="file" name="file" required="required" onChange={this.handleFileChange}/>
+            <input type="submit" value="Guardar" className="btn btn-primary btn-block btn-large"/>
+            </p>
+
+        </form>
       </div>
     );
   }
