c614b662bf9094316b64ecd8549e9483248b83c3
AliTosJM <alitosjm@gmail.com>
2021-05-24 01:42:53 -0500

    component graph

---
+++ src/components/AllPost/Graphs/Graphs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyGraph = ({ imageUrl }) => {
+  return (
+    <div className='content'>
+      <div className='card'>
+        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
+      </div>
+    </div>
+  );
+}
+export default MyGraph;
