90dfcd006669f77d3b408f868816c42a06ae9add
AliTosJM <alitosjm@gmail.com>
2021-10-10 23:56:17 -0500

    added robot

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -26,9 +26,22 @@ const Detail = props => {
         switch(id) {
           case 0:
             return(
-              <div>
-                <h1>Hi</h1>
-                <button onClick={() => alert("Hello!")}>Say Hello</button>
+              <div className={"robots"}>
+                <div className={"android"}>
+                  <div className={"head eyes"}>
+                    <div className={"left_eye"} />
+                    <div className={"right_eye"}/>
+                  </div>
+                  <div className={"upper_body"}>
+                    <div className={"left_arm"} />
+                    <div className={"torso"}/>
+                    <div className={"right_arm"}/>
+                  </div>
+                  <div className={"lower_body"}>
+                    <div className={"left_leg"} /> 
+                    <div className={"right_leg"} />                 
+                  </div>
+                </div>
               </div>
             );
           case 1:
@@ -84,8 +97,7 @@ const Detail = props => {
         </section>        
 
         <main>
-        {renderParagraphs}
-        
+          {renderParagraphs}
         </main>
 
         {renderSwitch(postId)}
